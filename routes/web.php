8afd9dd7ec21c6b9abd0497d1c1b064af080e831
Carlos E <carlos821031@gmail.com>
2022-12-21 12:28:04 -0500

    Acceso solo a los registrados

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware('auths');
+/*
+Route::get('/', function () {
+    return view('auth.login');
 });
+*/
 
 Route::middleware([
     'auth:sanctum',
